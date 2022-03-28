1b206068adb0894f787c7515d2fc58d4fa12ddaa
Leandro Correa <leandrocorrea1989@hotmail.com>
2022-03-28 15:29:32 -0300

    semana 1 sprites

---
+++ Assets/Scripts/ShipController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class ShipController : MonoBehaviour
+{
+    private new Rigidbody2D rigidbody;
+    private Vector2 movement;
+    public float speed = 1f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        movement = new Vector2(moveHorizontal, 0f);
+    }
+
+    // FixedUpdate se llama en cada fixed framerate frame (50ps default)
+    void FixedUpdate()
+    {
+        rigidbody.AddForce(movement * speed * 5f);
+    }
+}
